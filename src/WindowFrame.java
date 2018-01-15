c2e709da10e42912228e8eb471f8c96b987c0f55
Hong Sung <profsung@gmail.com>
2018-01-15 16:01:11 -0600

    lambda function as listener

--- src/WindowFrame.java
+++ src/WindowFrame.java
@@ -1,8 +1,6 @@
 
 import java.awt.BorderLayout;
 import java.awt.Container;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
